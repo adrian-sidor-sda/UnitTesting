0f7a0c991e3ae859eff3240db3806b114d4dab4c
Adrian Sidor <adriansidor11@wp.pl>
2020-01-09 19:15:00 +0100

    Fibonacci

---
+++ src/main/java/fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+package fibonacci;
+
+public class Fibonacci {
+
+    public int compute(int n){
+        if ((n==1)||(n==2))
+            return 1;
+        else
+            return compute(n-1)+compute(n-2);
+    }
+}
